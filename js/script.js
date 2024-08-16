d8c1987202bfb2e69d7fc8042dc11c8368ab84b3
devdxd <devdxd084@gmail.com>
2024-08-16 23:09:55 +0700

    add script for audio play btn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* ---------- POPUP -------------- */
+
 var menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 
@@ -17,4 +19,19 @@ function togglemenu2() {
 function togglemenu3() {
   var popupH = document.getElementById('popupH');
   popupH.classList.toggle('active')
+}
+
+/*------------- AUDIO ------------*/
+
+var song = document.getElementById('song');
+var iconSong = document.getElementById('icon-song');
+
+iconSong.onclick = function() {
+  if (song.paused) {
+      song.play();
+      iconSong.src = '../img/pause.png';
+  } else {
+      song.pause();
+      iconSong.src = '..img/play.png';
+  }
 }
