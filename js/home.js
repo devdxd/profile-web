84f1c473f88f639d8719205ee99c2ea41dff8052
devdxd <devdxd084@gmail.com>
2024-08-18 00:05:38 +0700

    rename from script.js to home.js

---
+++ js/home.js
@@ -0,0 +1,22 @@
+/* ---------- HOME POPUP -------------- BENERIN !!!!!!! */ 
+
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+        
+function togglemenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "130px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+        
+function togglemenu2() {
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+}
+        
+function togglemenu3() {
+  var popupH = document.getElementById('popupH');
+  popupH.classList.toggle('active')
+}
