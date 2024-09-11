fbeef515179a2983bb25a24e8f147fa0ef5e4855
senzxc <devdxd084@gmail.com>
2024-09-11 13:30:03 +0700

    add popup-img function

--- js/blog-gfx.js
+++ js/blog-gfx.js
@@ -11,3 +11,25 @@ prev.addEventListener('click', function() {
     let slides = document.querySelectorAll('.slides');
     slider.prepend(slides[slides.length - 1]);
 })
+
+document.querySelectorAll('.slides').forEach(image =>{
+    image.onclick = () => {
+        let imgUrl = image.style.getPropertyValue('--img');
+
+        imgUrl = imgUrl.slice(5, -2);
+
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = imgUrl;
+    }
+})
+
+document.querySelector('.popup-img span').onclick = () => {
+    document.querySelector('.popup-img').style.display = 'none';
+}
+
+document.querySelector('.popup-img').onclick = (e) => {
+    // Pastikan klik tidak terjadi pada elemen gambar
+    if (e.target === document.querySelector('.popup-img')) {
+        document.querySelector('.popup-img').style.display = 'none';
+    }
+};
